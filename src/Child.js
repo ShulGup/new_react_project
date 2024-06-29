f0ccbf72ccbd39d6c90efe69a01539f801f0a229
shulgup <shulbhi1810@gmail.com>
2024-06-29 15:52:15 +0530

    feat:feature_1_changes

--- src/Child.js
+++ src/Child.js
@@ -3,9 +3,7 @@ import React from "react";
 const Child = ({ name, surname }) => {
   return (
     <span>
-      <h1>
-        `${name} ${surname}`
-      </h1>
+      <h1>{`${name} ${surname}`}</h1>
     </span>
   );
 };
